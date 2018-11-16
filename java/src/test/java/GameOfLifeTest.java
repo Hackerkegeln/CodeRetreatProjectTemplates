8f4a6d83d12472ed9917727bba8cbb9338b7a0aa
Julian Voigt <baratock@hotmail.de>
2018-11-16 21:17:53 +0100

    java project

---
+++ java/src/test/java/GameOfLifeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GameOfLifeTest {
+    @Test
+    public void testShouldFail() {
+        assertEquals(false, true  );
+    }
+}
+
